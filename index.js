e36bdb662b2134eb00ebe12215c34553f636709a
DavidTurkGT <dlturk747@gmail.com>
2017-06-20 12:48:55 -0400

    Added mustache framework

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mustacheExpress = require("mustache-express");
+
 const app = express();
 
+const data = require("./data");
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
 app.get("/",function(req, res){
-  res.send("Hello!");
-})
+  res.render("index",
+  {
+    value: "Hello There!"
+  }
+  );
+});
+
+app.get("/directory", function(req, res){
+
+});
 
 app.listen(3000,function(){
   console.log("App running on localhost:3000");
