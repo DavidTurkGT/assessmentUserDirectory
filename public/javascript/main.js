4abdbee360a410af682b6e875483974fe259d16d
DavidTurkGT <dlturk747@gmail.com>
2017-06-20 14:55:37 -0400

    Added javascript functionality to load and hide full info cards

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+function loadCard(card){
+  document.querySelector(".index").classList.toggle("hidden");
+  card.classList.toggle("hidden");
+
+}
+
+function hideCard(card){
+  card.classList.toggle("hidden");
+  document.querySelector(".index").classList.toggle("hidden");
+}
