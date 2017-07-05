a2e502e88a866dd9810527df4f7a29b9fdda0ed0
DavidTurkGT <dlturk747@gmail.com>
2017-07-05 14:39:37 -0400

    Added a layout with nav bar

--- index.js
+++ index.js
@@ -8,31 +8,95 @@ let data = {};
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
+app.set('layout','layout');
 
 app.use(express.static("public"));
 
-//Setting up MongoDB
-var MongoClient = require('mongodb').MongoClient
+const getAllData = (req, res, next) =>{
+  //Setting up MongoDB
+  var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/userDirectory';
+  // Connection URL
+  var url = 'mongodb://localhost:27017/userDirectory';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  getData(db, () => {
-    db.close();
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    getData(db, () => {
+      db.close();
+      next();
+    });
   });
-});
 
-var getData = function(db, callback) {
-  let users = db.collection('users');
-  let promise = users.find({}).toArray().then( (users) => {
-    data = users;
-  })
+  var getData = function(db, callback) {
+    let users = db.collection('users');
+    users.find({}).toArray().then( (users) => {
+      data = users;
+      callback();
+    })
+  }
 }
+
+const getUnemployed = (req, res, next) => {
+  //Setting up MongoDB
+  var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+  // Connection URL
+  var url = 'mongodb://localhost:27017/userDirectory';
+
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    getData(db, () => {
+      db.close();
+      next();
+    });
+  });
+
+  var getData = function(db, callback) {
+    console.log("Running getData...");
+    let users = db.collection('users');
+    let promise = users.find({job: null}).toArray().then( (users) => {
+      data = users;
+      console.log("Data stored");
+      callback();
+    })
+  }
+};
+
+const getEmployed = (req, res, next) => {
+  //Setting up MongoDB
+  var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+  // Connection URL
+  var url = 'mongodb://localhost:27017/userDirectory';
+
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    getData(db, () => {
+      db.close();
+      next();
+    });
+  });
+
+  var getData = function(db, callback) {
+    console.log("Running getData...");
+    let users = db.collection('users');
+    let promise = users.find({job: {$not: {$eq: null}} }).toArray().then( (users) => {
+      data = users;
+      console.log("Data stored");
+      callback();
+    })
+  }
+};
+
 
 app.get("/", function(req, res){
   res.redirect("/directory");
@@ -40,8 +104,16 @@ app.get("/", function(req, res){
   // res.send("Data received!");
 });
 
-app.get("/directory", function(req, res){
-  res.render("index", {users: data});
+app.get("/directory", getAllData, function(req, res){
+  res.render("index", {title: "All users", users: data});
+});
+
+app.get("/directory/forhire", getUnemployed, (req, res) => {
+  res.render("index", {title: "Available for Hire", users: data});
+});
+
+app.get("/directory/employed", getEmployed, (req, res) => {
+  res.render("index", {title: "Employed", users: data});
 });
 
 app.listen(3000,function(){
