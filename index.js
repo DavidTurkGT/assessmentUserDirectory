43d9fb774821db39d754491f313b53cdb6027669
DavidTurkGT <dlturk747@gmail.com>
2017-07-05 14:57:08 -0400

    Can filter by country and skillset

--- index.js
+++ index.js
@@ -116,6 +116,72 @@ app.get("/directory/employed", getEmployed, (req, res) => {
   res.render("index", {title: "Employed", users: data});
 });
 
+app.get("/directory/:topic/:filter", (req, res) => {
+  switch (req.params.topic) {
+    case "country":
+      title = "Users from " + req.params.filter;
+      //Setting up MongoDB
+      var MongoClient = require('mongodb').MongoClient
+      , assert = require('assert');
+
+      // Connection URL
+      var url = 'mongodb://localhost:27017/userDirectory';
+
+      // Use connect method to connect to the server
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        getData(db, () => {
+          db.close();
+          res.render("index",{title: title, users: data});
+        });
+      });
+
+      var getData = function(db, callback) {
+        console.log("Running getData...");
+        let users = db.collection('users');
+        let promise = users.find({"address.country": req.params.filter}).toArray().then( (users) => {
+          data = users;
+          console.log("Data stored");
+          callback();
+        })
+      }
+      break;
+    case "skills":
+      title = "Users with skill: " + req.params.filter;
+      //Setting up MongoDB
+      var MongoClient = require('mongodb').MongoClient
+      , assert = require('assert');
+
+      // Connection URL
+      var url = 'mongodb://localhost:27017/userDirectory';
+
+      // Use connect method to connect to the server
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        getData(db, () => {
+          db.close();
+          res.render("index",{title: title, users: data});
+        });
+      });
+
+      var getData = function(db, callback) {
+        console.log("Running getData...");
+        let users = db.collection('users');
+        let promise = users.find({"skills": req.params.filter}).toArray().then( (users) => {
+          data = users;
+          console.log("Data stored");
+          callback();
+        })
+      }
+      break;
+    default:
+      res.redirect("/directory");
+      break;
+  }
+});
+
 app.listen(3000,function(){
   console.log("App running on localhost:3000");
 })
